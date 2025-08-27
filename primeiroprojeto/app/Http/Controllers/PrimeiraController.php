e6a4c0fdd67cd07577ef54ed1d4397110df04996
Alex Abreu <alexfavero4036@gmail.com>
2025-08-26 21:16:54 -0300

    primeiro_projeto

---
+++ primeiroprojeto/app/Http/Controllers/PrimeiraController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PrimeiraController extends Controller
+{
+    public function teste()
+    {
+        return "teste em laravel";
+    }
+
+    public function abrirForm()
+    {
+        return view("exemplo");
+    }
+
+    public function resposta(Request $request)
+    {
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+        $soma = $valor1 + $valor2;
+        return "Soma " . $soma;
+    }
+}
